a7b12c8c86b04ed66d775099ed83d658d585795c
Zosia-Sham <shamina_z@list.ru>
2016-12-17 21:51:45 +0300

    Logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,11 +1,18 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        LOG.debug("redirect to userList");
+        //request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");
     }
 }
